158051af18a9fd1aebd279c67adadd471beb8790
dinhooliveira <oliveiracienciacomputacao@gmail.com>
2019-11-07 14:16:34 -0300

    build

---
+++ precache-manifest.2e5470c8700b00c8e5443079ca377811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b53c4cbcc69d37d58d96485e2bdcaa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293c174987d96af7d82a",
+    "url": "/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "7f7159a6d70edfd1c8d8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "293c174987d96af7d82a",
+    "url": "/static/js/2.3ee0547d.chunk.js"
+  },
+  {
+    "revision": "7f7159a6d70edfd1c8d8",
+    "url": "/static/js/main.ca1d613a.chunk.js"
+  },
+  {
+    "revision": "6933bb5574204ec8d192",
+    "url": "/static/js/runtime-main.fd57687a.js"
+  }
+]);
